43834b407443502a197676a03edeeb749e4fc829
Shivam Bhasin <bhasinshivam2002@gmail.com>
2022-01-06 17:14:33 +0530

    changed the select sort algorithm tob e faster

--- src/utilityFunctions/sortingAlgorithms.js
+++ src/utilityFunctions/sortingAlgorithms.js
@@ -4,12 +4,13 @@ module.exports.selectSort = (arr) => {
 	for (let i = 0; i < newArray.length - 1; i++) {
 		let min_idx = i;
 		for (let j = i + 1; j < newArray.length; j++) {
-			if (newArray[i] > newArray[j]) {
-				animations.push({ classname: 'compare', pos: [i, j] });
+			if (newArray[min_idx] > newArray[j]) {
+				animations.push({ classname: 'compare', pos: [min_idx, j] });
+				min_idx = j;
 			}
 		}
-		[newArray[j], newArray[i]] = [newArray[i], newArray[j]];
-		animations.push({ classname: 'swap', pos: [i, j] });
+		animations.push({ classname: 'swap', pos: [i, min_idx] });
+		[newArray[i], newArray[min_idx]] = [newArray[min_idx], newArray[i]];
 		animations.push({ classname: 'sorted', pos: [i] });
 	}
 	animations.push({ classname: 'sorted', pos: [newArray.length - 1] });
